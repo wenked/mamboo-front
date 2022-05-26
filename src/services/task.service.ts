644655f7495b1d301e542752efdca4d4938c61c2
wenked <wenked.matheus@yahoo.com.br>
2022-05-26 11:25:54 -0300

    criando services

---
+++ src/services/task.service.ts
@@ -0,0 +1,38 @@
+import api from "./api";
+
+interface InputTask {
+	description: string;
+	name: string;
+	status: "done" | "in progress" | "pending" | "testing";
+}
+
+interface UpdateInputTask {
+	_id: string;
+	description?: string;
+	name?: string;
+	status?: "done" | "in progress" | "pending" | "testing";
+}
+
+interface GetInputTasks {
+	status?: "done" | "in progress" | "pending" | "testing";
+}
+
+export const createTaskService = async (input: InputTask) => {
+	const { data } = await api.post("/create", input);
+	return data;
+};
+
+export const updateTaskService = async (input: UpdateInputTask) => {
+	const { data } = await api.post(`/update/${input._id}`, input);
+	return data;
+};
+
+export const getTasksService = async (input: GetInputTasks) => {
+	const { data } = await api.get("/list", { params: input });
+	return data;
+};
+
+export const deleteTaskService = async (_id: string) => {
+	const { data } = await api.delete(`/delete/${_id}`);
+	return data;
+};
